f3ed2b04426afd69ca0000449b63d376d7eece63
SnackCode <dylan.frankland@gmail.com>
2014-05-07 17:13:39 -0700

    Changed to be more general for others to use
    Changed after successful test on Magento 1.8

--- app/code/community/SnackCode/OrderCancelEmailNotification/Model/Observer.php
+++ app/code/community/SnackCode/OrderCancelEmailNotification/Model/Observer.php
@@ -13,7 +13,7 @@ class SnackCode_OrderCancelEmailNotification_Model_Observer
         function sendEmail($subject,$body){
             $mail = Mage::getModel('core/email');
             $mail->setToName(Mage::getStoreConfig('trans_email/ident_support/name'));
-            $mail->setToEmail('dylan@prosoundcommunications.com');//(Mage::getStoreConfig('trans_email/ident_support/email'));
+            $mail->setToEmail(Mage::getStoreConfig('trans_email/ident_support/email'));
             $mail->setFromEmail(Mage::getStoreConfig('trans_email/ident_support/email'));
             $mail->setFromName(Mage::app()->getStore()->getName());
             $mail->setBody($body);
@@ -31,10 +31,10 @@ class SnackCode_OrderCancelEmailNotification_Model_Observer
         
         /*log every success or issue using email, mage log at /var/log/system.log, and also the order comments*/
         $subject = 'Order #'.$orderId.' Was Cancelled. Do not ship.';
-        $body = '<h1>Please click <a href="http://192.168.1.72:8069/?db=PCI#page=0&limit=80&view_type=list&model=sale.order&menu_id=297&action=374">here</a> to find the order.</h1>';
+        $body = 'Order #'.$orderId.' Was Cancelled. Do not ship.';
         sendEmail($subject,$body);
         Mage::log($oid.$subject,Zend_Log::DEBUG,'OrderCancelEmailNotification.log',true);
-        $order->addStatusHistoryComment($subject.$body, true);
+        $order->addStatusHistoryComment('Cancel Email Sent to '.Mage::getStoreConfig('trans_email/ident_support/email'), true);
         
         return $this;
     }
