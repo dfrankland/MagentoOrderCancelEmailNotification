884d1ce481a6349bdb1b4f9382e7dced023fe53e
SnackCode <dylan.frankland@gmail.com>
2014-05-08 10:12:23 -0700

    Oops
    This joke is getting old. Anyways, I changed an old variable ($oid ->
    $orderId) and also made a function protected to prevent redeclares.

--- app/code/community/SnackCode/OrderCancelEmailNotification/Model/Observer.php
+++ app/code/community/SnackCode/OrderCancelEmailNotification/Model/Observer.php
@@ -2,6 +2,26 @@
 /*Written by Dylan Frankland - dylan.frankland+SnackCode@gmail.com*/
 class SnackCode_OrderCancelEmailNotification_Model_Observer
 {
+    /*email function to alert of failed captures*/
+    protected function sendEmail($subject,$body){
+        $mail = Mage::getModel('core/email');
+        $mail->setToName(Mage::getStoreConfig('trans_email/ident_support/name'));
+        $mail->setToEmail(Mage::getStoreConfig('trans_email/ident_support/email'));
+        $mail->setFromEmail(Mage::getStoreConfig('trans_email/ident_support/email'));
+        $mail->setFromName(Mage::app()->getStore()->getName());
+        $mail->setBody($body);
+        $mail->setSubject($subject);
+        $mail->setType('html');// You can use 'html' or 'text'
+        try
+        {
+            $mail->send();
+        }
+        catch (Exception $e)
+        {
+            Mage::logException('Magento Order: Cancel Notification // Exception message: '.$e->getMessage());
+        }
+    }
+    
     public function orderCancelNotification($observer)
     {
         /*get all order details from the observer*/
@@ -9,31 +29,11 @@ class SnackCode_OrderCancelEmailNotification_Model_Observer
         $order = $event->getOrder();
         $orderId = $event->getOrder()->getIncrementId();
         
-        /*email function to alert of failed captures*/
-        function sendEmail($subject,$body){
-            $mail = Mage::getModel('core/email');
-            $mail->setToName(Mage::getStoreConfig('trans_email/ident_support/name'));
-            $mail->setToEmail(Mage::getStoreConfig('trans_email/ident_support/email'));
-            $mail->setFromEmail(Mage::getStoreConfig('trans_email/ident_support/email'));
-            $mail->setFromName(Mage::app()->getStore()->getName());
-            $mail->setBody($body);
-            $mail->setSubject($subject);
-            $mail->setType('html');// You can use 'html' or 'text'
-            try
-            {
-                $mail->send();
-            }
-            catch (Exception $e)
-            {
-                Mage::logException('Magento Order: Cancel Notification // Exception message: '.$e->getMessage());
-            }
-        }
-        
         /*log every success or issue using email, mage log at /var/log/system.log, and also the order comments*/
         $subject = 'Order #'.$orderId.' Was Cancelled. Do not ship.';
         $body = 'Order #'.$orderId.' Was Cancelled. Do not ship.';
-        sendEmail($subject,$body);
-        Mage::log($oid.$subject,Zend_Log::DEBUG,'OrderCancelEmailNotification.log',true);
+        $this->sendEmail($subject,$body);
+        Mage::log($orderId.$subject,Zend_Log::DEBUG,'OrderCancelEmailNotification.log',true);
         $order->addStatusHistoryComment('Cancellation email sent to '.Mage::getStoreConfig('trans_email/ident_support/email'), true);
         
         return $this;
